ec21de5170261dcfe5a689a274e5044833b7b85d
harry-fruit <isaqueduarte17@gmail.com>
2022-01-11 11:27:24 -0300

    adding some changes to Update method

--- src/modules/user/handlers/updateUser.ts
+++ src/modules/user/handlers/updateUser.ts
@@ -1,8 +1,10 @@
+import { UpdateHandlerResponseType } from "../../../utils/UpdateHandlerResponse";
 import { UpdateUserDto } from "../dto/updateUser.dto";
 import { UserEntity } from "../entities/User.entity";
 
-export const updateUser = async (payload: UpdateUserDto) => {
+export const updateUser = async (payload: UpdateUserDto): Promise<UpdateHandlerResponseType>  => {
   const { rg, ...fieldsToUpdate } = payload;
   const response = await UserEntity.update(fieldsToUpdate, { where: { rg } });
+  console.log(response)
   return response;
 };
