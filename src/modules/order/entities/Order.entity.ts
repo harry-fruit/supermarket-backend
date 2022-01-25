e27114b6fdb46bdf26b2f421b6ca6c8ed9888cb7
Isaque D <isaqueduarte17@gmail.com>
2022-01-25 00:18:43 -0300

    to fix

--- src/modules/order/entities/Order.entity.ts
+++ src/modules/order/entities/Order.entity.ts
@@ -19,7 +19,7 @@ export const OrderEntity = DbInstance.define(
     totalPrice: {
       type: DataType.DOUBLE,
       allowNull: false,
-      defaultValue: 0.0,
+      defaultValue: 0.1,
     },
     // productsId: {
     //   type: DataType.INTEGER,
