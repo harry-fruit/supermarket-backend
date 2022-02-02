78990ca921768123a56695959a29c7eeb53eda2b
harry-fruit <isaqueduarte17@gmail.com>
2022-02-02 00:36:22 -0300

    add some features

--- src/modules/product/handlers/getProduct.ts
+++ src/modules/product/handlers/getProduct.ts
@@ -1,31 +1,14 @@
-// import { Model } from "sequelize/dist";
-// import { HttpResponse } from "../../../utils/HttpResponse";
-// import { FindAllFilter } from "../../../utils/types/FindAllFilter.type";
-// import { ProductEntity } from "../entities/Product.entity";
-// import { ProductInterface } from "../interfaces/Product.interface";
+import { Model } from "sequelize/dist";
+import { ProductEntity } from "../entities/Product.entity";
+import { ProductInterface } from "../interfaces/Product.interface";
 
-// export const GetAllProducts = async (findParams: FindAllFilter): Promise<Model<ProductInterface>[]> => {
-//     try {
-//       const { limit, currentPage } = findParams;
+export const GetProductById = async (searchKey: string): Promise<Model<ProductInterface> | null> => {
+    try{
+      const user: Model<ProductInterface> | null = await ProductEntity.findOne({ where: { Cpf } });
+      
+      return user;
   
-//           const allProducts = await ProductEntity.findAll({
-//               limit: limit || 10,
-//               offset: (limit || 10) * (currentPage - 1)
-//           });
-          
-//           return allProducts;
-//       } catch(error: any) {
-//           throw HttpResponse('Internal Server Error', 500, error);
-//       }
-//   }
-  
-//   export const getProduct = async (rg: string): Promise<Model<ProductInterface>> => {
-  
-//       const product: Model<ProductInterface> | null = await ProductEntity.findOne({ where: { rg } });
-  
-//       if (!product){
-//         throw "Product doesn't exist";
-//       };
-  
-//       return product;
-//   };
+    } catch (error) {
+      throw error;
+    };
+  };
--- src/modules/product/router.ts
+++ src/modules/product/router.ts
@@ -1,20 +1,81 @@
 import { Request, Response, Router } from "express";
-import { HttpResponse } from "../../utils/HttpResponse";
+import { Model } from "sequelize/dist";
+import { HttpResponse, HttpResponseType } from "../../utils/HttpResponse";
 import { CreateProduct } from "./handlers/CreateProduct";
 import { ProductInterface } from "./interfaces/Product.interface";
+import { StatusCodes as HttpStatusCode , ReasonPhrases as HttpStatus } from 'http-status-codes';
+import { ErrorHandler } from "../../utils/ErrorHandler";
+import { GetProductById } from "./handlers/GetProduct";
 
 export const productRouter: Router = Router();
 
-productRouter.post(
-  "/create-product",
-  async (request: Request, response: Response): Promise<void> => {
-    try {
-        const product = await CreateProduct(request.body as ProductInterface);
-        const handledResponse = HttpResponse("created", 201, product);
-        response.send(handledResponse);
-
-    } catch (error: any) {
-        throw new Error(error);
+productRouter.post("/create-product", async (request: Request, response: Response): Promise<void> => {
+  try {
+    const payload = request.body as ProductInterface;
+    
+    const handledResponse: Model<ProductInterface> | string = await CreateProduct(payload);
+    
+    if(typeof handledResponse == 'string'){
+      
+      const responsePayload: HttpResponseType = HttpResponse(HttpStatus.NOT_ACCEPTABLE, HttpStatusCode.NOT_ACCEPTABLE, handledResponse);
+      
+      response
+        .status(HttpStatusCode.NOT_ACCEPTABLE)
+        .send(responsePayload);
+        return;
     }
-  }
-);
+
+    const responsePayload: HttpResponseType = HttpResponse(HttpStatus.CREATED, HttpStatusCode.CREATED, handledResponse);
+
+    response
+      .status(HttpStatusCode.CREATED)
+      .send(responsePayload);
+    
+    return;
+  
+    } catch (error: any) {
+      throw ErrorHandler(HttpStatus.INTERNAL_SERVER_ERROR, 500, error);
+  };
+});
+
+productRouter.get("/get-product", async (request: Request, response: Response) => {
+  try {
+    let { UniqueCode, Id } = request.query;
+    
+    if (!UniqueCode || !Id) {
+      const message = "Please, insert a value to Id or Unique Code."
+      const responsePayload = HttpResponse(HttpStatus.BAD_REQUEST, HttpStatusCode.BAD_REQUEST, message);
+
+      response
+        .status(HttpStatusCode.BAD_REQUEST)
+        .send(responsePayload);
+      
+      return;
+    }
+
+    Id         = Id ? Id.toString() : '';
+    UniqueCode = UniqueCode.toString();
+
+    if (Id) {
+      const handledResponse: Model<ProductInterface> | null = await GetProductById(Id);
+    } else if (UniqueCode) {
+      const handledResponse: Model<ProductInterface> | null = await GetProductById(Id);
+    }
+
+    if(!handledResponse) {
+      const responsePayload = HttpResponse(HttpStatus.NOT_FOUND, HttpStatusCode.NOT_FOUND, {})
+      response
+        .status(HttpStatusCode.NOT_FOUND)
+        .send(responsePayload);
+      
+      return;
+    }
+
+    const responsePayload: HttpResponseType = HttpResponse(HttpStatus.OK, HttpStatusCode.OK, handledResponse);
+
+    response.send(responsePayload);
+    
+  } catch (error: any) {
+    throw ErrorHandler(HttpStatus.INTERNAL_SERVER_ERROR, 500, error)
+  };
+})
